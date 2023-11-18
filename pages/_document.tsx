cbc12f03bb8a67b013d6922b620ea0073f2282dd
Mat5588 <65315400+mister5588@users.noreply.github.com>
2023-11-18 10:16:14 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/navigation/Header";
-import { FlipProvider } from "@rngstudio/flip";
 
 export default function Document() {
   return (
@@ -13,12 +12,6 @@ export default function Document() {
         />
       </Head>
       <body>
-        <FlipProvider
-          partnerId="5fVWRf1AAhMkrr8mYbej8NjYQkzcz7uTmkoNiW9Wp5M5"
-          network={process.env.NEXT_PUBLIC_NETWORK}
-        >
-          <Header />
-        </FlipProvider>
         <Main />
         <NextScript />
         <Footer />
